370ed0efa657bb15e5e8dde2bd9c908b1a0d67b5
SumedhChobitkar <sumedhchobitkar04@gmail.com>
2025-07-08 18:15:20 +0530

    added status functionality in helpDesk.

--- _HRMS Application/HRMS-Application/src/main/java/com/example/HRMS/Application/Repository/HelpDeskRepository.java
+++ _HRMS Application/HRMS-Application/src/main/java/com/example/HRMS/Application/Repository/HelpDeskRepository.java
@@ -1,7 +1,15 @@
 package com.example.HRMS.Application.Repository;
 
 import com.example.HRMS.Application.Entity.HelpDesk;
+import com.example.HRMS.Application.Entity.HelpDeskStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface HelpDeskRepository extends JpaRepository<HelpDesk, Long> {
+
+    List<HelpDesk> findByHelpDeskStatus(HelpDeskStatus helpDeskStatus);
+
+    List<HelpDesk> findByEmployeeId(Long employeeId);
+    List<HelpDesk> findByEmployeeFirstNameAndEmployeeLastName(String firstName, String lastName);
 }
